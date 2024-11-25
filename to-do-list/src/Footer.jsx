2f15069843ff0e45a0642d61da83b6a678327877
Kshitij <160639154+KshitijKhande@users.noreply.github.com>
2024-11-25 22:09:49 +0530

    Add files via upload

---
+++ to-do-list/src/Footer.jsx
@@ -0,0 +1,13 @@
+import React from "react";
+//import DeleteIcon from "@mui/icons-material/Delete";
+
+function Footer() {
+  const year = new Date().getFullYear();
+  return (
+    <footer>
+      <p>Copyright ⓒ {year}</p>
+    </footer>
+  );
+}
+
+export default Footer;
---
+++ to-do-list/src/Header.jsx
@@ -0,0 +1,12 @@
+import React from "react";
+//import HighlightIcon from "@mui/icons-material/Highlight";
+
+function Header() {
+  return (
+    <header>
+      <h1>Your's To Do List</h1>
+    </header>
+  );
+}
+
+export default Header;
